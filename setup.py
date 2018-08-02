91c7d07dec4d3885b889f0bcf7b0f364e0d3361d
Jay R Bolton <jayrbolton@gmail.com>
2018-08-02 11:40:17 -0700

    Add support for building the package with conda

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='kbase_workspace_utils',
+    version='0.0.1',
+    description='KBase Workspace object downloaders and uploaders',
+    author='KBase Team',
+    author_email='scanon@lbl.gov',
+    url='https://github.com/kbase/kbase_workspace_utils',
+    package_dir={'': 'src'},
+    packages=['kbase_workspace_utils'],
+    python_requires='>=3'
+)
