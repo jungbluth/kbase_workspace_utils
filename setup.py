22bd916cb663a0f33a7f2da55b981733e28eaef5
Jay R Bolton <jayrbolton@gmail.com>
2018-08-06 14:34:51 -0700

    Add requirements and increment version

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from setuptools import setup
 
 setup(
     name='kbase_workspace_utils',
-    version='0.0.4',
+    version='0.0.5',
     description='KBase Workspace object downloaders and uploaders',
     author='KBase Team',
     author_email='scanon@lbl.gov',
     url='https://github.com/kbase/kbase_workspace_utils',
     package_dir={'': 'src'},
     packages=['kbase_workspace_utils'],
+    install_requires=[
+        'python-dotenv==0.8.2',
+        'requests==2.19.1',
+        'biopython==1.72'
+    ],
     python_requires='>=3'
 )
